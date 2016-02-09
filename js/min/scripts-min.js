8ccc21cbfa5abad150b973af497b0365a3ec5329
Dan Sly <git@dansly.com>
2016-02-09 16:43:45 +0000

    First Commit

---
+++ js/min/scripts-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function($){$(".type-it").typeIt({strings:["designer","developer","animator"],speed:150,breakLines:!1,breakDelay:1500,lifeLike:!0,cursor:!0,loop:!0,startDelay:1e3})});
--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,14 @@
 $(document).ready(function($) {
-    $('.hider').click(function() {
-        $('.subtext').toggleClass("show");
-    });
-    $('.about').click(function() {
-        $('.about-panel').toggleClass("show-about");
-        $('.page-fade').toggleClass("faded");
-    });
-    $('.about-close').click(function() {
-        $('.about-panel').toggleClass("show-about");
-        $('.page-fade').toggleClass("faded");
-    });
-});
-
-
-$(document).keydown(function(e) {
-    if (e.keyCode == 27) {
-      $('.about-panel').removeClass("show-about");
-      $('.page-fade').removeClass("faded");
-    }
-});
-
-
-
-$( document ).ready(function() {
-    $('.main-gallery').flickity({
-  // options
-  cellAlign: 'left',
-  contain: 'true',
-  watchCSS: 'fallbackOn',
-  percentPosition: 'false',
-  wrapAround: 'true',
-  arrowShape: {
-  x0: 0,
-  x1: 50, y1: 50,
-  x2: 55, y2: 50,
-  x3: 5
-}
-});
-});
-
-
-
-$(document).ready(function () {
-  $('.accordion-tabs-minimal').each(function(index) {
-    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open');
+ $('.type-it').typeIt({
+    strings: ['designer', 'developer', 'animator'],
+    speed: 150,
+    breakLines: false,
+    breakDelay: 1500,
+    lifeLike: true,
+    cursor: true,
+    loop: true,
+    startDelay: 1000
   });
-  $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs-minimal');
-      accordionTabs.find('.is-open').removeClass('is-open');
 
-      $(this).next().toggleClass('is-open');
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-    } else {
-      event.preventDefault();
-    }
-  });
 });
 
-
-var container = $('div').offset().top;
-    $(document).on("scroll", function(){
-    if
-      ($(document).scrollTop() > container){
-      $(".class").addClass("shrink");
-    }
-    else
-    {
-      $(".class").removeClass("shrink");
-    }
-  });
